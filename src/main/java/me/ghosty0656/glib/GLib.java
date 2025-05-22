dfd1a0384bb1a13ae0a58bac1bd70b29906dca42
ghosty0656 <ghosty0656.smth@gmail.com>
2025-05-22 11:18:51 +0200

    Initial Commit

---
+++ src/main/java/me/ghosty0656/glib/GLib.java
@@ -0,0 +1,17 @@
+package me.ghosty0656.glib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

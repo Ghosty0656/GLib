8a159026f601a83af10640ff4dc872340c2b4ea1
ghosty0656 <ghosty0656.smth@gmail.com>
2025-05-22 20:41:46 +0200

    Import Uniform and create an instance for each GPlugin instance

--- src/main/java/me/ghosty0656/glib/GPlugin.java
+++ src/main/java/me/ghosty0656/glib/GPlugin.java
@@ -1,5 +1,6 @@
 package me.ghosty0656.glib;
 
+import net.william278.uniform.paper.PaperUniform;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +12,7 @@ import java.util.*;
 
 public abstract class GPlugin extends JavaPlugin implements Listener {
 
+    protected PaperUniform commands;
     private final Set<String> expectedWorlds = new HashSet<>();
     private final Set<String> loadedWorlds = new HashSet<>();
     private boolean allWorldsLoaded = false;
@@ -27,6 +29,8 @@ public abstract class GPlugin extends JavaPlugin implements Listener {
 
     @Override
     public final void onEnable() {
+        commands = PaperUniform.getInstance(this);
+
         Bukkit.getPluginManager().registerEvents(this, this);
 
         // Get all worlds, loaded or not
@@ -44,6 +48,8 @@ public abstract class GPlugin extends JavaPlugin implements Listener {
     @EventHandler
     public final void onWorldLoad(WorldLoadEvent event) {
         loadedWorlds.add(event.getWorld().getName());
+
+        // If all worlds are loaded, call the onWorldsLoaded function
         if (loadedWorlds.containsAll(expectedWorlds) && !allWorldsLoaded) {
             allWorldsLoaded = true;
             onWorldsLoaded();

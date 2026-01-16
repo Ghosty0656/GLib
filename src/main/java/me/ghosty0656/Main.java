eca8144c2f4ef050a47fe7687fd5d59a8ba761eb
ghosty0656 <ghosty0656.smth@gmail.com>
2026-01-16 11:08:05 +0100

    Add the base for the plugin and pack

--- src/main/java/me/ghosty0656/Main.java
+++ src/main/java/me/ghosty0656/Main.java
@@ -1,7 +1,28 @@
 package me.ghosty0656;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class Main extends JavaPlugin {
+
+    public static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public Main(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    public void setup() {
+    }
+
+    @Override
+    protected void start() {
+    }
+
+    @Override
+    protected void shutdown() {
     }
 }
